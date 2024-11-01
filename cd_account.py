cebfbee8457b4caa4e90cd586836d60df785ad2f
GerardoCastanos <gerardo.castanos@gmail.com>
2024-11-01 19:58:53 -0400

    Update functions

--- cd_account.py
+++ cd_account.py
@@ -1,8 +1,10 @@
 """Import the Account class from the Account.py file."""
 # ADD YOUR CODE HERE
+from Account import Account
 
 def create_cd_account(balance, interest_rate, months):
-    """Creates a CD account, calculates interest earned, and updates the account balance.
+    """Creates a CD account, calculates interest earned, and updates 
+    the account balance.
 
     Args:
         balance (float): The initial CD account balance.
@@ -13,21 +15,41 @@ def create_cd_account(balance, interest_rate, months):
         float: The updated CD account balance after adding the interest earned.
         And returns the interest earned.
     """
-    # Create an instance of the `Account` class and pass in the balance and interest parameters.
+    # Create an instance of the `Account` class and pass in the balance 
+    # and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-
+    cd_account = Account(balance,0)
     # Calculate interest earned
     # ADD YOUR CODE HERE
-
+    interest_earned = balance * (interest_rate/100) * (months/12)
     # Update the CD account balance by adding the interest earned
     # ADD YOUR CODE HERE
-
-    # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
+    updated_balance = balance + interest_earned
+    # Pass the updated_balance to the set balance method using the instance 
+    # of the CD Account class.
     # ADD YOUR CODE HERE
-
-    # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
+    cd_account.set_balance(updated_balance)
+    # Pass the interest_earned to the set interest method using the instance 
+    # of the CD Account class.
     # ADD YOUR CODE HERE
-
+    cd_account.set_interest(interest_earned)
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return cd_account.balance, cd_account.interest # ADD YOUR CODE HERE
+
+if __name__ == "__main__":
+    print("*" * 79)
+    i_Balance = 1000
+    i_interest = 0
+    k_InterestRate = 2
+    m_months = 24
+
+    My_Account = Account(i_Balance,i_interest)
+    print(f"My initial account balance is: {My_Account.balance}")
+    print(f"My initial interest is: {My_Account.interest}")
+    My_Account = Account(
+        *create_cd_account(i_Balance, k_InterestRate, m_months)
+    )
+    print(f"My final account balance is: {My_Account.balance}")
+    print(f"My final interest is: {My_Account.interest}")
+    print("*" * 79)
